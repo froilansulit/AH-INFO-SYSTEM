f58ab1c8502418369d9ee4158f1d5b4551f6f142
froilansulit <froilansulit017@gmail.com>
2022-06-14 23:13:05 +0800

    commit

--- pages/users/check_availability.php
+++ pages/users/check_availability.php
@@ -6,10 +6,10 @@ if(!empty($_POST["username"])) {
   $result = mysqli_query($conn,$query);
   $count = mysqli_num_rows($result);
   if($count > 0) {
-    echo "<span style='color:red'> * Sorry username already used.</span>";
+    echo "<span style='color:red'> * username already taken.</span>";
     echo "<script>$('#addUser').prop('disabled',true);</script>";
   }else{
-    echo "<span style='color:green'> username available for user .</span>";
+    echo "<span style='color:green'> username is available.</span>";
     // echo "<script>$('#addUser').prop('disabled',false);</script>";
   }
 } 
