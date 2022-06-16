11508a0ed1c56cafe0723bb99a8a0bce88f4abf2
froilansulit <froilansulit017@gmail.com>
2022-06-17 00:41:31 +0800

    commit

--- pages/session.php
+++ pages/session.php
@@ -1,13 +1,9 @@
 <?php 
 session_start();
 
-
-
 $username = $_SESSION['username'];
 $acc_name = $_SESSION['acc_name'];
 $user_type = $_SESSION['user_type'];
-
-
 
 if (!isset($_SESSION['username'])) {
     session_destroy();
@@ -18,7 +14,6 @@ if (!isset($_SESSION['user_type'])) {
     session_destroy();
     header('location: ../login/'); 
 }
-
 
 // if(isset($_POST['logout_btn'])) {
 
