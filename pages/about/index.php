28bfcba6d440e705911f59172bfa80ac636798cf
froilansulit <froilansulit017@gmail.com>
2022-06-29 00:08:32 +0800

    mdfield_end

--- pages/about/index.php
+++ pages/about/index.php
@@ -26,7 +26,6 @@ include '../head.php'; ?>
       <!-- partial -->
       <div class="main-panel" >
         <div class="content-wrapper" style="background-color:#bddcff;">
-          
           <div class="row">
             <div class="col-md-6 grid-margin stretch-card">
               <div class="card">
@@ -34,16 +33,12 @@ include '../head.php'; ?>
                   <p class="card-title text-md-center text-xl-left">About</p>
                   <div class=" flex-wrap justify-content-between justify-content-md-center justify-content-xl-between align-items-center">
                    <div class="text-center my-5">
-                      
                       <span class="company_name alert-dark p-4  rounded-pill">A.H Araullo & Sons Rizal Slipways Inc.</span>
                       <p>Shipyard in Navotas</p>
-                      
                    </div>
                 <div class="row">
-              
                 <div class="align-self-center mt-5">
                     <p class="lead mb-3">
-
                     <i class="ti-location-pin icon"></i>
                       <span class="lead">940 M Naval St. San Jose Navotas City</span>
                     </p>
@@ -57,7 +52,6 @@ include '../head.php'; ?>
                     </p>
                 </div>
                 </div>
-
                   </div>  
                 </div>
               </div>
@@ -65,7 +59,6 @@ include '../head.php'; ?>
             <div class="col-md-6 grid-margin stretch-card">
               <div class="card">
                 <div class="card-body">
-                  
                   <p class="card-title text-md-center text-xl-left">Map</p>
                   <div class=" flex-wrap justify-content-between justify-content-md-center justify-content-xl-between align-items-center">
                       <img src="../../images/AH_Map.jpg" class="img-fluid" alt="" srcset="">
@@ -89,6 +82,5 @@ include '../head.php'; ?>
 
 <?php include '../scripts.php'; ?>
 </body>
-
 </html>
 
