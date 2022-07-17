c7ae47f4f34d1a549e80954e3b0d3c2c760c1f8a
froilansulit <froilansulit017@gmail.com>
2022-07-17 23:40:44 +0800

    commit

--- pages/sidebar.php
+++ pages/sidebar.php
@@ -32,23 +32,23 @@
                   </a>
               </li>
               <?php if ($user_type == "admin") {
-                  echo '<li class="nav-item">
+                    echo '<li class="nav-item">
                   <a class="nav-link" href="../users/">
                       <i class="ti-user menu-icon"></i>
                       <span class="menu-title">Users</span>
                   </a>
               </li>';
-              } ?>
-               <li class="nav-item">
+                } ?>
+              <li class="nav-item">
                   <a class="nav-link" href="../about/">
                       <i class="ti-info menu-icon"></i>
                       <span class="menu-title">About</span>
                   </a>
               </li>
               <li class="nav-item fixed-bottom col-md-2 pb-3 pr-1">
-                <a class="nav-link bg-muted  text-dark mb-3" >
-                     <span class="menu-title text-capitalize font-weight-bold"><?php echo $acc_name; ?></span><span class="badge badge-pill badge-dark text-capitalize float-right ml-1"><?php echo $user_type; ?></span>
-                 </a>
+                  <a class="nav-link bg-muted  text-dark mb-3">
+                      <span class="menu-title text-capitalize font-weight-bold"><?php echo $acc_name; ?></span><span class="badge badge-pill badge-dark text-capitalize float-right ml-1"><?php echo $user_type; ?></span>
+                  </a>
                   <!-- <a class="nav-link btn btn-danger text-dark bg-danger rounded-pill border border-none " href="../logout.php">
                       <i class="ti-power-off text-dark"></i>
                       <span class="menu-title mx-auto">LOGOUT</span>
