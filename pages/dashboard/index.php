3aaf4fd8c22a7bb5ab5fd8732b3ff6ce90dafe0d
froilan <froilansulit017@gmail.com>
2022-07-27 22:45:04 -0700

    commit

--- pages/dashboard/index.php
+++ pages/dashboard/index.php
@@ -213,7 +213,7 @@ const data2 = {
   ],
   datasets: [{
     label: 'My First Dataset',
-    data: [<?php echo $last_month_total; ?>, <?php echo $this_month_total; ?>],
+    data: [<?= $last_month_total; ?>, <?php echo $this_month_total; ?>],
     backgroundColor: [
       'rgb(54, 162, 235)',
       'rgb(255, 205, 86)'
