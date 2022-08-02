715f6aff028b27b33c9945119d46e3899e0f9731
froilan <froilansulit017@gmail.com>
2022-08-02 22:25:07 +0800

    commit

--- pages/drydock/index.php
+++ pages/drydock/index.php
@@ -23,8 +23,6 @@
   if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (isset($_POST['save_drydock'])) {
 
-      
-      
       // $image_id = uniqid();
       $company_name = htmlspecialchars($_POST['company_name']);
       $ship_name = htmlspecialchars($_POST['ship_name']);
