47f7c3d1818344ac3624975ef2d526ea68f39f80
froilan <froilansulit017@gmail.com>
2022-08-15 00:34:13 +0800

    🧹 Refractoring Code 👨🏽‍💻

--- pages/drydock/index.php
+++ pages/drydock/index.php
@@ -171,10 +171,10 @@ if (isset($_SESSION['error'])) { ?>
                                                     <span aria-hidden="true">&times;</span>
                                                 </button>
                                             </div>
-                                        <?php
-                                            unset($_SESSION['error']);
-                                        }
-                                        ?>
+<?php
+unset($_SESSION['error']);
+}
+?>
                                         <table id="example1" class="table table-hover" style="width:100%">
                                             <thead style="font-size:10px" class="text-center">
                                                 <tr>
