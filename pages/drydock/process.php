5a21fd39078340c5c7a4bc590cb21e6eb468532b
froilan <froilansulit017@gmail.com>
2022-08-17 21:01:10 +0800

    🧹 Refractoring Code 👨🏽‍💻

--- pages/drydock/process.php
+++ pages/drydock/process.php
@@ -29,16 +29,12 @@ if (isset($_POST['userID'])) {
     $id = $_POST['userID'];
     
     $sql = "select * from drydock_record where id='$id'"; // select all the data in DB
-
     $result = mysqli_query($conn, $sql); // query to get the data
 
-    while ($row = mysqli_fetch_assoc($result)) {
-
-      ?>
+    while ($row = mysqli_fetch_assoc($result)) { ?>
       <img src="upload/<?php echo $row['images']; ?>" class="img-fluid" alt="">
       <?php
     }
-
 }
 
 
