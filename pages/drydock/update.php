77dbee803ea56058ad54399de8f741498e08439c
froilan <froilansulit017@gmail.com>
2022-08-20 21:58:47 +0800

    🧹 Refractoring Code 👨🏽‍💻

--- pages/drydock/update.php
+++ pages/drydock/update.php
@@ -22,21 +22,21 @@
     $CDT = date($Format);
     $FDT = date($Format, strtotime("+$FD days +$FM months +$FY years"));
 
-if (isset($_POST['data_btn'])) {
+    if (isset($_POST['data_btn'])) {
 
-$drydock_ID = $_POST['update_id'];
+    $drydock_ID = $_POST['update_id'];
 
-$sql_update = "select * from drydock_record where id=$drydock_ID"; // select all the data in DB
-$update_result = mysqli_query($conn, $sql_update); // query to get the data
-$update_row = mysqli_fetch_assoc($update_result);
+    $sql_update = "select * from drydock_record where id=$drydock_ID"; // select all the data in DB
+    $update_result = mysqli_query($conn, $sql_update); // query to get the data
+    $update_row = mysqli_fetch_assoc($update_result);
 
-$Get_Cname = $update_row['Company_Name'];
-$Get_ShipName = $update_row['Ship_Name'];
-$Get_LotNum = $update_row['Lot_Num'];
-$Get_DryDate = $update_row['Drydock_date'];
-$Get_ExpDate = $update_row['Exp_Departure'];
-$dry_image = $update_row['images'];
-}
+    $Get_Cname = $update_row['Company_Name'];
+    $Get_ShipName = $update_row['Ship_Name'];
+    $Get_LotNum = $update_row['Lot_Num'];
+    $Get_DryDate = $update_row['Drydock_date'];
+    $Get_ExpDate = $update_row['Exp_Departure'];
+    $dry_image = $update_row['images'];
+    }
 
 if (empty($drydock_ID)) {
 header('location: ../drydock/');
