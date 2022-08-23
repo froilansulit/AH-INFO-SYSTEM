cf88f17d7c90fd5d949dbbec5daf692c0202b3f7
froilan <froilansulit017@gmail.com>
2022-08-23 19:47:44 +0800

    🧹 Refractoring Code 👨🏽‍💻

--- pages/dashboard/index.php
+++ pages/dashboard/index.php
@@ -84,9 +84,9 @@ $this_year_total = $Y_inc_rec - $Y_out_rec;
 
 <body>
     <div class="container-scroller">
-        <?php include '../navbar.php'; ?>
+<?php include '../navbar.php'; ?>
         <div class="container-fluid page-body-wrapper">
-            <?php include '../sidebar.php'; ?>
+<?php include '../sidebar.php'; ?>
             <div class="main-panel">
                 <div class="content-wrapper" style="background-color:#bddcff;">
                     <div class="row">
