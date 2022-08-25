55c6fd8a5e22296f0d03629482bbf27a63906de6
froilan <froilansulit017@gmail.com>
2022-08-25 23:22:32 +0800

    🧹 Refractoring Code 👨🏽‍💻

--- pages/dashboard/index.php
+++ pages/dashboard/index.php
@@ -152,7 +152,7 @@
                         $month = date('F');
                         $year = date('Y');
 
-                        $sql = "select * from financial_record"; // select all the data in DB
+                        $sql = "SELECT * FROM financial_record"; // select all the data in DB
                         $result = mysqli_query($conn, $sql); // query to get the data
 
                         while ($row = mysqli_fetch_assoc($result)) {
