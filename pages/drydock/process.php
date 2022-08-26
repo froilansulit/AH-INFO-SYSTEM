ede4742508b321d5e94b9fe11cd4d7c78d11c230
froilan <froilansulit017@gmail.com>
2022-08-26 18:18:44 +0800

    🧹 Refractoring Code 👨🏽‍💻

--- pages/drydock/process.php
+++ pages/drydock/process.php
@@ -1,55 +1,55 @@
 <?php 
-include '../connect.php';
+    include '../connect.php';
 
-// for deleting
-if (isset($_POST['deleteSend'])) {
+    // for deleting
+    if (isset($_POST['deleteSend'])) {
+        
+        $unique = mysqli_real_escape_string($conn, $_POST['deleteSend']);
+        $dry_query = "SELECT * FROM drydock_record WHERE id='$unique'";
+        $dry_query_run = mysqli_query($conn, $dry_query);
     
-    $unique = mysqli_real_escape_string($conn, $_POST['deleteSend']);
-    $dry_query = "SELECT * FROM drydock_record WHERE id='$unique'";
-    $dry_query_run = mysqli_query($conn, $dry_query);
-  
-    while ($row = mysqli_fetch_assoc($dry_query_run)) {
-        if ($img_path = "upload/" . $row['images']) {
-            $sql = "DELETE FROM drydock_record WHERE id=$unique";
-            $result = mysqli_query($conn, $sql);
-  
-            if ($result) {
-                unlink($img_path);
-                # keep it blank ajax will do the success message
-            } else {
-                die(mysqli_error($conn));
+        while ($row = mysqli_fetch_assoc($dry_query_run)) {
+            if ($img_path = "upload/" . $row['images']) {
+                $sql = "DELETE FROM drydock_record WHERE id=$unique";
+                $result = mysqli_query($conn, $sql);
+    
+                if ($result) {
+                    unlink($img_path);
+                    # keep it blank ajax will do the success message
+                } else {
+                    die(mysqli_error($conn));
+                }
             }
         }
     }
-  }
 
-//   for drydock image preview
-if (isset($_POST['userID'])) {
+    //   for drydock image preview
+    if (isset($_POST['userID'])) {
 
-    $id = $_POST['userID'];
-    $sql = "SELECT * FROM drydock_record WHERE id='$id'"; // select all the data in DB
-    $result = mysqli_query($conn, $sql); // query to get the data
+        $id = $_POST['userID'];
+        $sql = "SELECT * FROM drydock_record WHERE id='$id'"; // select all the data in DB
+        $result = mysqli_query($conn, $sql); // query to get the data
 
-    while ($row = mysqli_fetch_assoc($result)) { ?>
-      <img src="upload/<?php echo $row['images']; ?>" class="img-fluid" alt="">
-      <?php
+        while ($row = mysqli_fetch_assoc($result)) { ?>
+        <img src="upload/<?php echo $row['images']; ?>" class="img-fluid" alt="">
+        <?php
+        }
     }
-}
 
-// for drydock viewing details
-if (isset($_POST['viewID'])) {
-    $user_id = $_POST['viewID'];
+    // for drydock viewing details
+    if (isset($_POST['viewID'])) {
+        $user_id = $_POST['viewID'];
 
-    $sql = "SELECT * FROM drydock_record WHERE id=$user_id";
-    $result = mysqli_query($conn,$sql);
-    $response = array();
-    while ($row = mysqli_fetch_assoc($result)) {
-       $response = $row;
+        $sql = "SELECT * FROM drydock_record WHERE id=$user_id";
+        $result = mysqli_query($conn,$sql);
+        $response = array();
+        while ($row = mysqli_fetch_assoc($result)) {
+        $response = $row;
+        }
+        echo json_encode($response);
     }
-    echo json_encode($response);
-}
-else {
-    $response['status'] = 200;
-    $response['message'] = "Invalid or Data not found";
-}
+    else {
+        $response['status'] = 200;
+        $response['message'] = "Invalid or Data not found";
+    }
 ?>
