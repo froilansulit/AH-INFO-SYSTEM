1bebf4ab85207e675da4123a49968dbed756cda3
froilan <froilansulit017@gmail.com>
2022-08-29 01:20:10 +0800

    🧹 Refactoring Code 👨🏽‍💻

--- pages/login/index.php
+++ pages/login/index.php
@@ -156,7 +156,7 @@
   </div>
 <?php include '../scripts.php'; ?>
   <script src="../app.js"></script>
-  <?php
+<?php
 // for login function 
 if (isset($_POST["lgnLogin"])) {
   $username = mysqli_real_escape_string($conn, $_POST["username"]);
