1e8b2724abd0460819c260c14bf562874a177e7d
froilan <froilansulit017@gmail.com>
2022-09-01 21:45:28 +0800

    🧹 Refactoring Code 👨🏽‍💻

--- pages/Montly_Tally/index.php
+++ pages/Montly_Tally/index.php
@@ -148,9 +148,7 @@
         <?php include '../modals.php'; ?>
       </div>
     </div>
-    <!-- page-body-wrapper ends -->
   </div>
-  <!-- container-scroller -->
   <?php include '../scripts.php'; ?>
 
   
