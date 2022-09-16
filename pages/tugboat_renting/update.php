b62bc9e87efdcf3dd3ec0a87c280660003b7b6b7
froilan <froilansulit017@gmail.com>
2022-09-16 23:33:31 +0800

    Refratoring Codes

--- pages/tugboat_renting/update.php
+++ pages/tugboat_renting/update.php
@@ -115,8 +115,11 @@
             </div>
           </div>
         </div>
-        <?php include '../footer.php'; ?>
-        <?php include '../modals.php'; ?>
+        <?php 
+        include '../footer.php';
+        include '../modals.php';
+         ?>
+        <?php  ?>
       </div>
     </div>
   </div>
