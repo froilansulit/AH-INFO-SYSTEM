29e9e0178b1846d8bd26138c48ccf83ccd2e8c40
froilan <froilansulit017@gmail.com>
2022-09-19 00:07:14 +0800

    🧹 Refactoring Codes 👨🏽‍💻

--- pages/users/check_availability.php
+++ pages/users/check_availability.php
@@ -6,10 +6,12 @@ if (!empty($_POST["username"])) {
 	$query = "SELECT * FROM users WHERE username='" . $username . "'";
 	$result = mysqli_query($conn, $query);
 	$count = mysqli_num_rows($result);
+
 	if ($count > 0) {
 		echo "<span style='color:red'> * username already taken.</span>";
 		echo "<script>$('#addUser').prop('disabled',true);</script>";
-	} else {
+	} 
+	else {
 		echo "<span style='color:green'> username is available.</span>";
 		// echo "<script>$('#addUser').prop('disabled',false);</script>";
 	}
