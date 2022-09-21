2f165be95feff9df8d06142c025776ff27dfaeb8
froilan <froilansulit017@gmail.com>
2022-09-21 22:59:02 +0800

    🧹 Refactoring Codes 👨🏽‍💻

--- pages/users/index.php
+++ pages/users/index.php
@@ -69,15 +69,8 @@
           </div>
         </div>
       </div>
-      <!-- /.modal-content -->
     </div>
-    <!-- /.modal-dialog -->
   </div>
-
-  <!-- end of rent modal  -->
-
-  <!-- start of view modal  -->
-
   <div class="modal fade" id="VViewUser">
 
     <div class="modal-dialog">
