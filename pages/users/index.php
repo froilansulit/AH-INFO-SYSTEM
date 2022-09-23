2931a937635bf809cca1e1ab5873d73118c61e0a
froilan <froilansulit017@gmail.com>
2022-09-23 23:36:29 +0800

    🧹 Refactoring Codes 👨🏽‍💻

--- pages/users/index.php
+++ pages/users/index.php
@@ -153,8 +153,6 @@
               <button type="button" class="btn btn-default btn-rounded" data-dismiss="modal">Cancel</button>
               <input type="hidden" id="hiddenUserData">
             </div>
-
-            <!-- <button type="button" class="btn btn-default btn-rounded" data-dismiss="modal">Cancel</button> -->
           </div>
           <!-- /.card-body -->
 
