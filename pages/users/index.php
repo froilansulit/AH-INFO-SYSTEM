6eb7f6c0c87ff9931da8d1d9540b72e12ae73db0
froilan <froilansulit017@gmail.com>
2022-09-23 23:37:55 +0800

    🧹 Refactoring Codes 👨🏽‍💻

--- pages/users/index.php
+++ pages/users/index.php
@@ -189,7 +189,6 @@
   {
 ?>
                       <div class="alert alert-success border border-muted alert-dismissible fade show" role="alert">
-                        <!-- <strong>Holy guacamole!</strong> -->
                         <?php echo $_SESSION['status']; ?>
                         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                           <span aria-hidden="true">&times;</span>
