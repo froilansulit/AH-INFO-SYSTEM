6e4d3dc8827909b5df814185688b184cf4156b36
froilan <froilansulit017@gmail.com>
2022-09-27 12:36:54 +0800

    🧹 Refactoring Codes 👨🏽‍💻

--- pages/users/view_data.php
+++ pages/users/view_data.php
@@ -1,9 +1,7 @@
 <?php 
     include '../connect.php';
 
-
     // for data viewing 
-
     if (isset($_POST['viewID'])) {
         $user_id = $_POST['viewID'];
 
@@ -11,12 +9,12 @@
         $result = mysqli_query($conn,$sql);
         $response = array();
         while ($row = mysqli_fetch_assoc($result)) {
-        $response = $row;
+            $response = $row;
         }
         echo json_encode($response);
     }
     else {
         $response['status'] = 200;
         $response['message'] = "Invalid or Data not found";
-}
+    }
 ?>
