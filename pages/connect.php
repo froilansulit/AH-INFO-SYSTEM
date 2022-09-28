e9a2694e77367a9cb372ad983f3de4bba0fab9bd
froilan <froilansulit017@gmail.com>
2022-09-28 23:42:34 +0800

    🧹 Refactoring Codes 👨🏽‍💻

--- pages/connect.php
+++ pages/connect.php
@@ -5,7 +5,5 @@ $password = '';
 $db_name = 'ah_info_system';
 
 $conn = new mysqli($server,$username, $password, $db_name); // for connection
-if (!$conn) {
-    die(mysqli_error($conn));
-}
+if (!$conn) {  die(mysqli_error($conn)); }
 ?>
