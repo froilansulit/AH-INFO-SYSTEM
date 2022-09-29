9547a9c67aa1d806653cc996f508b7dd42f62633
froilan <froilansulit017@gmail.com>
2022-09-29 18:55:51 +0800

    🧹 Refactoring Codes 👨🏽‍💻

--- pages/scripts.php
+++ pages/scripts.php
@@ -1,22 +1,12 @@
   <script src="../../vendors/base/vendor.bundle.base.js"></script>
-  <!-- endinject -->
-  <!-- Plugin js for this page-->
-  <!-- End plugin js for this page-->
-  <!-- inject:js -->
   <script src="../../js/off-canvas.js"></script>
   <script src="../../js/hoverable-collapse.js"></script>
   <script src="../../js/template.js"></script>
   <script src="../../js/todolist.js"></script>
-  <!-- endinject -->
-  <!-- Custom js for this page-->
-
-  <!-- End custom js for this page-->
-
   <script src="../../js/chart.js"></script>
   <!-- End custom js for this page-->
   <script src="../../vendors/chart.js/Chart.min.js"></script>
   <!-- End plugin js for this page-->
-
   <!-- <script src="../../js/app.js"></script> -->
   <!-- <script src="../../js/jquery.min.ajx.js"></script> -->
   <script src="../../js/sweetalert2.all.min.js"></script>
