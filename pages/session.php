c2639b0c566dd33cb05d87e8da7fa190050578e5
froilan <froilansulit017@gmail.com>
2022-10-18 23:31:19 +0800

    🧹 Refactoring Codes 👨🏽‍💻

--- pages/session.php
+++ pages/session.php
@@ -1,16 +1,16 @@
 <?php 
-session_start();
-$username = $_SESSION['username'];
-$acc_name = $_SESSION['acc_name'];
-$user_type = $_SESSION['user_type'];
+    session_start();
+    $username = $_SESSION['username'];
+    $acc_name = $_SESSION['acc_name'];
+    $user_type = $_SESSION['user_type'];
 
-if (!isset($_SESSION['username'])) {
-    session_destroy();
-    header('location: ../login/');	
-}
+    if (!isset($_SESSION['username'])) {
+        session_destroy();
+        header('location: ../login/');	
+    }
 
-if (!isset($_SESSION['user_type'])) {
-    session_destroy();
-    header('location: ../login/'); 
-}
+    if (!isset($_SESSION['user_type'])) {
+        session_destroy();
+        header('location: ../login/'); 
+    }
 ?>
