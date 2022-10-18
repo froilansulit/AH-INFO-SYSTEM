1c2e1e7591246e88892ae0a2dd4ff891bdaab960
froilan <froilansulit017@gmail.com>
2022-10-18 23:31:43 +0800

    🧹 Refactoring Codes 👨🏽‍💻

--- pages/sidebar.php
+++ pages/sidebar.php
@@ -38,12 +38,12 @@
                   </a>
               </li>' ?>
 <?php } ?>
-              <li class="nav-item">
+              <!-- <li class="nav-item">
                   <a class="nav-link" href="../about/">
                       <i class="ti-info menu-icon"></i>
                       <span class="menu-title">About</span>
                   </a>
-              </li>
+              </li> -->
               <li class="nav-item fixed-bottom col-md-2 pb-3 pr-1">
                   <a class="nav-link bg-muted  text-dark mb-3">
                       <span class="menu-title text-capitalize font-weight-bold"><?= $acc_name; ?></span><span class="badge badge-pill badge-dark text-capitalize float-right ml-1"><?= $user_type; ?></span>
