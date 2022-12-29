79cd48bd0cc41652538eb1c6742f5241a83f68de
Froilan Quiseo Sulit <froilansulit017@gmail.com>
2022-12-29 14:14:06 +0800

    🛠 Fixing Some Contents

--- pages/modals.php
+++ pages/modals.php
@@ -40,7 +40,6 @@
                                 <label for="Discount Price">Discount Price</label>
                                 <input class="form-control" type="text" name="currency-field" id="currency-field" pattern="^\P\d{1,3}(,\d{3})*(\.\d+)?P" value="" data-type="currency" placeholder="P 0.00">
                             </div>
-                            
                             <div class="form-group col-md-4">
                                 <label for="Expiry Date">Expiry Date</label>
                                 <input type="text" class="form-control" id="Expiry Date" placeholder="Expiry Date">
@@ -49,8 +48,6 @@
                     <!-- <button type="submit" class="btn btn-primary btn-rounded" >Save</button> -->
 
                     <!-- <button type="submit" class="btn btn-primary btn-rounded" name="submit">Save</button> -->
-
-
                     <button type="button" class="btn btn-primary btn-rounded" id="addIncoming" disabled>Save</button>
 
                     <button type="button" class="btn btn-default btn-rounded" data-dismiss="modal">Cancel</button>
